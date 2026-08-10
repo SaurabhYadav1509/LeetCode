76aac44e46b6bb72359dbc542e2c8a39c153e9e6
SaurabhYadav1509 <saurabhyadav8423002224@gmail.com>
2026-08-10 14:29:28 +0530

    Time: 7 ms (90.88%), Space: 35.1 MB (20.31%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+bool isValid(vector<int>& nums, int k, int target, int n){
+    int stud=1,sumi=0;
+
+    for (int i=0; i<n; i++){
+    // one element itself is more than target        
+    if (nums[i] > target) return false; 
+    if (sumi+nums[i] <= target) sumi += nums[i];
+    else {
+        stud++;
+        sumi = nums[i];
+    }        
+}
+    return stud > k ? false : true;
+}
+
+    int shipWithinDays(vector<int>& nums, int k) {
+       int n = nums.size();
+        if (k > n) return false;
+        int sum = 0;
+        for (int i=0;i<n;i++) sum += nums[i];
+
+        int start = *max_element(nums.begin(), nums.end());;
+        int end = sum;
+        int ans = -1;
+
+        // apply binary search for the range
+        while(start <= end ){
+            int mid = start + (end - start) /2;
+        // find minimum if u got a min one
+            if (isValid(nums,k,mid,n)){
+                ans = mid;
+                end = mid-1;
+            } 
+        // invalid case => range is not get
+            else start = mid+1;
+        }
+        return ans;        
+    }
+};
