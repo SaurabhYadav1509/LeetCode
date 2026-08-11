d3e7e209b2da51d541afae2e1d646d5e3856aebe
SaurabhYadav1509 <saurabhyadav8423002224@gmail.com>
2026-08-12 01:08:29 +0530

    Time: 0 ms (100%), Space: 45.2 MB (25.43%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -6,11 +6,12 @@ public:
         int maxi = INT_MIN;
         int currMin =0;
         int n = nums.size();
+        // use kadane's for maximum +ve sum and maximum -ve sum
         for (int i=0; i<n ; i++){
            currMax += nums[i];
            maxi = max(currMax,maxi);
            if (currMax < 0) currMax =0;
-
+        // reset to 0 if current sum becomes -ve => maxi or +ve => mini 
            currMin += nums[i];
            mini = min(currMin ,mini);
            if (currMin > 0) currMin =0;           
