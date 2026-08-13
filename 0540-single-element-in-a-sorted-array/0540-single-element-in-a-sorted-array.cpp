98bc696aa45e3b62e9610b1fd83fb1ceff85242e
SaurabhYadav1509 <saurabhyadav8423002224@gmail.com>
2026-08-13 22:46:26 +0530

    Time: 0 ms (100%), Space: 26.3 MB (6.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,57 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        for(int i=0;i<nums.size();i++){
-         ans = ans ^ nums[i];
+
+        if (nums.size() == 1)
+            return nums[0];
+
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // First element
+            if (mid == 0) {
+                if (nums[mid] != nums[mid + 1])
+                    return nums[mid];
+
+                start = mid + 1;
+                continue;
+            }
+
+            // Last element
+            if (mid == nums.size() - 1) {
+                if (nums[mid] != nums[mid - 1])
+                    return nums[mid];
+
+                end = mid - 1;
+                continue;
+            }
+
+            // Single element
+            if (nums[mid - 1] != nums[mid] &&
+                nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+
+            // Even index
+            if (mid % 2 == 0) {
+                if (nums[mid - 1] == nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+
+            // Odd index
+            else {
+                if (nums[mid - 1] == nums[mid])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
         }
-        return ans;
+
+        return -1;
     }
 };
