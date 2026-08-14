f9a5a6b7affe282b8997b580a8ea7e4c1476c85d
SaurabhYadav1509 <saurabhyadav8423002224@gmail.com>
2026-08-14 07:54:07 +0530

    Time: 4 ms (28.78%), Space: 9.8 MB (28.94%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int maximumLengthSubstring(string s) {
         int n = s.size();
-        int left =0;
-        int maxLen =0;
+        int start =0;
+        int end =0;
+        int maxLen=0;
         unordered_map<char,int>mp;
 
-        for (int right=0 ; right<n ; right++){
-           mp[s[right]]++;
-           // occurences is twice
-           while(mp[s[right]] > 2){
-            mp[s[left]]--;
-            left++;
-           }
-           maxLen = max(maxLen,right-left+1);
+        while(end < n){
+            mp[s[end]]++;
+            
+// frequency of that char should not more than 2 (jispe s[end] point kr rha hai)
+            while(mp[s[end]] > 2){
+                mp[s[start]]--;
+                start++;
+            }
+            maxLen = max(maxLen,end-start+1);
+            end++;
         }
-        
         return maxLen;
     }
 };
