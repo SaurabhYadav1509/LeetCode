ace0f868759d12c58506a5e8e7642e02ec25583a
SaurabhYadav1509 <saurabhyadav8423002224@gmail.com>
2026-08-17 00:51:07 +0530

    Time: 52 ms (93.77%), Space: 148.5 MB (93.15%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -8,25 +8,34 @@ public:
     }
     
     void addNum(int num) {
-   if (left.size() == 0 || num < left.top()) left.push(num);
-   else right.push(num);
+        if (left.empty() || num < left.top())
+            left.push(num);
+        else
+            right.push(num);
 
-   if (left.size() > right.size()+1){
-    right.push(left.top());
-    left.pop();
-   }
+        // left has too many elements
+        if (left.size() > right.size() + 1) {
+            right.push(left.top());
+            left.pop();
+        }
 
-   if (right.size() > left.size()){
-    left.push(right.top());
-    right.pop();
-   } 
+        // right has too many elements
+        if (right.size() > left.size() + 1) {
+            left.push(right.top());
+            right.pop();
+        }
 }
     
-    double findMedian() {
-    if (left.size() == right.size()) 
-    return (left.top() + right.top()) /2.0;
-    else return left.top();
+double findMedian() {
+    if (left.size() == right.size())
+        return (left.top() + right.top()) / 2.0;
+
+    if (left.size() > right.size())
+        return left.top();
+
+    return right.top();
 }
+
 };
 
 /**
