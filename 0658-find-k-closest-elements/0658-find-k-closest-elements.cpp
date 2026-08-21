55258d46c0ff98ff05e59ee86d32c4067adca614
SaurabhYadav1509 <saurabhyadav8423002224@gmail.com>
2026-08-21 17:25:15 +0530

    Time: 54 ms (5.1%), Space: 44.3 MB (6.44%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
+typedef pair<int,int>pi;
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
-        // pair <ele,dist>  map fails for the dupliacte elements
-        // unordered_map<int,int>mp;
-        // for (auto ele : arr){
-        //     mp[ele] = abs(ele-x);
-        // }
+       int n  = arr.size();
 
+       vector<int>ans;
+       unordered_map<int,int>mp;
+        for (auto x : arr) mp[x]++;
 
-        // pq => <dist,ele>
-        typedef pair<int,int> pi;
-        priority_queue<pi>pq;
-        for (auto ele : arr){
-            int dist = abs(ele-x);
-            pq.push({dist,ele});
+       priority_queue<pi> pq;
+      
+       for (auto y : arr){
+        int ele = y;
+        int dist = abs(ele - x);
 
-            if (pq.size()>k) pq.pop();
-        }
+        pq.push({dist,ele});
+        if (pq.size() > k) pq.pop();
+       }
 
-        vector<int>ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+       while(!pq.empty()){
+        ans.push_back(pq.top().second);
+        pq.pop();
+       }
+
+       sort(ans.begin(),ans.end());
+       return ans;
     }
 };
